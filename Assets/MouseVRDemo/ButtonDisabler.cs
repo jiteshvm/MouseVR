197a8241558051c44a9acf482470b5c8fce0735b
Jitesh <jiteshvm@gmail.com>
2017-11-20 02:41:16 -0800

    first working version that can be used to conduct tests read settings from file , write output to file , controls to adjust the panel

--- Assets/MouseVRDemo/ButtonDisabler.cs
+++ Assets/MouseVRDemo/ButtonDisabler.cs
@@ -11,7 +11,7 @@ public class ButtonDisabler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        gameObject.GetComponent<InteractionButton>().OnPress.AddListener(OnButtonPress);
+        GetComponent<InteractionButton>().OnPress.AddListener(OnButtonPress);
         GameObject DemoManagerObj = GameObject.Find("DemoManager");
         DemoManagerRef = DemoManagerObj.GetComponent<DemoManager>();
         //Debug.Log(transform.parent.position.x + " , " + transform.parent.position.y + " , " + transform.parent.position.z);
@@ -31,22 +31,20 @@ public class ButtonDisabler : MonoBehaviour {
             InteractionHand ih = (InteractionHand)ic;
             if (ih && ih.isTracked && ih.contactingObjects.Count > 0)
             {
-                Vector tmp = ih.leapHand.Fingers[(int)Finger.FingerType.TYPE_INDEX].TipPosition;
-                //Vector3 fingerPos3 = tmp.ToVector3();
-                Vector2 fingerPos = new Vector2(tmp.x, tmp.y);
-                Vector2 objPos = new Vector2(transform.parent.position.x, transform.parent.position.y);
-                float dist = Vector2.Distance(fingerPos, objPos);
-                //float dist = Vector3.Distance(transform.parent.position, fingerPos3);
-                Debug.Log(dist * 1000.0f);
+                Vector TipPosition = ih.leapHand.Fingers[(int)Finger.FingerType.TYPE_INDEX].TipPosition;
+                Vector2 TipPosition2D = new Vector2(TipPosition.x, TipPosition.y);
+                Vector2 objPos2D = new Vector2(transform.parent.position.x, transform.parent.position.y);
+                float dist2D = Vector2.Distance(TipPosition2D, objPos2D);
+                float floatdist2D_mm = dist2D * 1000.0f;
+                DemoManagerRef.LogTimeAndDistance(floatdist2D_mm);
                 //DebugCube.transform.position = ih.leapHand.Fingers[(int)Finger.FingerType.TYPE_INDEX].TipPosition.ToVector3();
             }
         }
 
-        DemoManagerRef.ActivateRandomTarget();
+        
     }
 
 	// Update is called once per frame
 	void Update () {
-		
 	}
 }
--- Assets/MouseVRDemo/DemoManager.cs
+++ Assets/MouseVRDemo/DemoManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using Leap;
 using Leap.Unity;
@@ -33,46 +35,60 @@ public class DemoManager : MonoBehaviour {
 
     List<int> RandomButtonIndices;
 
+    List<float> TimeList;
+
+    List<float> DistanceList;
+
     GameObject InteractionManagerObj;
     public InteractionManager InteractionManagerRef;
 
-    GameObject DebugCube;
+    GameObject BackgroundPanel;
+
+    Transform CenterEyeTransform;
+
+    Vector3 EyeForwardDirection;
+
+    float DistanceFromEye = 0.3f;
+
+    float StartTime = 0.0f;
+
+    float EndTime;
+
+    float ElapsedTime;
 
     void Start () {
-
-        //DebugCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        //DebugCube.transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
-        //DebugCube.GetComponent<MeshRenderer>().material.color = Color.green;
+        //LinePoints = new List<Vector3>();
+        //LinePoints.Add(Vector3.zero);
+        //LinePoints.Add(Vector3.zero);
+        //LineDebug = new VectorLine("line", LinePoints, 4.0f);
+        //LineDebug.smoothWidth = true;
+        ReadSettingsFromFile();
+        BackgroundPanel = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        BackgroundPanel.transform.localScale = new Vector3(2.0f, 2.0f, 0.02f);
+        BackgroundPanel.GetComponent<MeshRenderer>().material.color = Color.green;
+        BackgroundPanel.GetComponent<MeshRenderer>().enabled = false;
+        Destroy(BackgroundPanel.GetComponent<BoxCollider>());
 
         VectorManager.useDraw3D = true;
-        LinePoints = new List<Vector3>();
-        LineDebug = new VectorLine("line", LinePoints, 4.0f);
-        LineDebug.smoothWidth = true;
+
+        List<Vector3> testPoints = new List<Vector3>();
 
         TargetWidthMeters = TargetWidth / 1000.0f;
         InterTargetDistanceMeters = InterTargetDistance / 1000.0f;
         TargetRadius = (InterTargetDistanceMeters * NumberOfTargets) / (2.0f * Mathf.PI);
+
+        //Debug.Log("TargetWidth : " + TargetWidth);
+        //Debug.Log("InterTargetDistance : " + InterTargetDistance);
+
         //Debug.Log("Outer Circle Radius : " + TargetRadius);
         //Debug.Log(TargetWidthMeters);        
         
+        ElapsedTime = 0.0f;
+
         InteractionManagerObj = GameObject.Find("Interaction Manager");
         if(InteractionManagerObj)
         {
             InteractionManagerRef = InteractionManagerObj.GetComponent<InteractionManager>();
-            /*
-            if(InteractionManagerRef)
-            {
-                foreach(InteractionController ic in InteractionManagerRef.interactionControllers)
-                {
-                    InteractionHand ih = (InteractionHand)ic;
-                    if(ih && ih.isRight) 
-                    {
-                        //Debug.Log(ih.leapHand.Fingers[Finger.FingerType.TYPE_INDEX].TipPosition);
-                        //ih.leapProvider.OnUpdateFrame += OnUpdateFrame;
-                    }
-                }
-            }
-            */
         }
         
         if(CenterObj && TargetPrefab && NumberOfTargets > 0)
@@ -81,6 +97,8 @@ public class DemoManager : MonoBehaviour {
             float AngleDelta = 2.0f * Mathf.PI / NumberOfTargets;
             TargetsList = new List<GameObject>();
             RandomButtonIndices = new List<int>();
+            TimeList = new List<float>();
+            DistanceList = new List<float>();
 
             for (int i = 0; i < NumberOfTargets; ++i)
             {
@@ -90,33 +108,91 @@ public class DemoManager : MonoBehaviour {
                 GameObject NewCircleObj = GameObject.Instantiate(TargetPrefab, new Vector3(x, y, z), Quaternion.identity);
                 NewCircleObj.name = "Button" + i;
                 NewCircleObj.transform.GetChild(0).gameObject.GetComponent<Rigidbody>().detectCollisions = false;
+                //NewCircleObj.transform.GetChild(0).gameObject.GetComponent<InteractionButton>().OnPress.AddListener(OnButtonPress);
                 NewCircleObj.transform.GetChild(0).GetChild(0).localScale = new Vector3(TargetWidthMeters, TargetWidthMeters, 0.01f);
+                NewCircleObj.transform.SetParent(BackgroundPanel.transform);
                 TargetsList.Add(NewCircleObj);
                 RandomButtonIndices.Add(i);
+                if( i > 0)
+                {
+                    testPoints.Add(TargetsList[i-1].transform.position);
+                    testPoints.Add(TargetsList[i].transform.position);
+                }
+                
                 CurrentAngle += AngleDelta;
             }
+            testPoints.Add(TargetsList[NumberOfTargets - 1].transform.position);
+            testPoints.Add(TargetsList[0].transform.position);
         }
 
-        //float dist = Vector3.Distance(TargetsList[1].transform.position, TargetsList[2].transform.position);
-        //Debug.Log(dist);
+        //VectorLine testLine = new VectorLine("test", testPoints, 8);
+        // for drawing lines between the targets
+        //testLine.Draw3D();
 
-        List<Vector3> testPoints = new List<Vector3>();
-        testPoints.Add(TargetsList[0].transform.position);
-        testPoints.Add(TargetsList[1].transform.position);
-        VectorLine testLine = new VectorLine("test", testPoints, 8);
-        testLine.Draw3D();
-
-
-        ActivateRandomTarget();
         LeapVRCameraControl.OnValidCameraParams += OnValidCameraParams;
         InputTracking.Recenter();
+        SetPanelDirectionAndPosition();
     }
+
+    void Restart()
+    {
+        RandomButtonIndices.Clear();
+        TimeList.Clear();
+        DistanceList.Clear();
+        for (int i = 0; i < TargetsList.Count; ++i)
+        {
+            TargetsList[i].transform.GetChild(0).gameObject.GetComponent<Rigidbody>().detectCollisions = false;
+            TargetsList[i].transform.GetChild(0).gameObject.GetComponent<SimpleInteractionGlow>().defaultColor = Color.black;
+            TargetsList[i].transform.GetChild(0).gameObject.GetComponent<SimpleInteractionGlow>().hoverColor = Color.black;
+            TargetsList[i].transform.GetChild(0).gameObject.GetComponent<SimpleInteractionGlow>().primaryHoverColor = Color.black;
+            RandomButtonIndices.Add(i);
+        }
+        ActivateRandomTarget();
+    }
+
+    private void ReadSettingsFromFile()
+    {
+        string file_path = "ConfigSettings.txt";
+        try
+        {
+            StreamReader file_reader = new StreamReader(file_path);
+            while(!file_reader.EndOfStream)
+            {
+                string input_line = file_reader.ReadLine( );
+                input_line = input_line.Trim();
+                string[] parameters = input_line.Split(':');
+                if(parameters.Length != 2)
+                    continue;
+                
+                parameters[0] = parameters[0].Trim();
+                parameters[1] = parameters[1].Trim();
+
+                if(parameters[0].Equals("TargetWidth", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    TargetWidth = (float) Convert.ToDouble(parameters[1]);
+                }
+                if(parameters[0].Equals("InterTargetDistance", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    InterTargetDistance = (float) Convert.ToDouble(parameters[1]);
+                }
+            }
+            file_reader.Close( );  
+        
+        }
+        catch(FileNotFoundException fe)
+        {
+            Debug.Log(fe.Message);
+        }
+        
+    }
+
 
     public void ActivateRandomTarget()
     {
         if(RandomButtonIndices.Count > 0)
         {
-            int RandomIndex = Random.Range(0, RandomButtonIndices.Count);
+            StartTime = ElapsedTime;
+            int RandomIndex = UnityEngine.Random.Range(0, RandomButtonIndices.Count);
             //Debug.Log("random index : " + RandomIndex);
             int RandomValue = RandomButtonIndices[RandomIndex];
             //Debug.Log(" random value : " + RandomValue);
@@ -133,33 +209,94 @@ public class DemoManager : MonoBehaviour {
 
     private void OnUpdateFrame(Frame frame)
     {
-        Debug.Log("test");
+        //Debug.Log("OnUpdateFrame");
     
     }
 
     private void OnValidCameraParams(LeapVRCameraControl.CameraParams cameraParams)
     {
-        //LineDebug.drawTransform = cameraParams.CenterEyeTransform;
+        CenterEyeTransform = cameraParams.CenterEyeTransform;
     }
 
+    void SetPanelDirectionAndPosition()
+    {
+        SetPanelPosition();
+        SetPanelDirection();
+    }
+
+    void SetPanelDirection()
+    {
+        if(BackgroundPanel && CenterEyeTransform)
+        {
+            EyeForwardDirection = CenterEyeTransform.forward;
+            BackgroundPanel.transform.LookAt((CenterEyeTransform.position + (EyeForwardDirection * 100.0f)));
+        }
+    }
+
+    void SetPanelPosition()
+    {
+        if(BackgroundPanel && CenterEyeTransform)
+        {
+            BackgroundPanel.transform.position = CenterEyeTransform.position + (EyeForwardDirection * DistanceFromEye);
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        //LineDebug.Draw3D();
+    }
     void Update()
     {
-        /*
-        if (InteractionManagerRef)
+        ElapsedTime += Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.C))
         {
-            foreach (InteractionController ic in InteractionManagerRef.interactionControllers)
-            {
-                InteractionHand ih = (InteractionHand)ic;
-                if (ih && ih.isTracked && ih.contactingObjects.Count > 0)
-                {
-                    Debug.Log(ih.leapHand.Fingers[(int)Finger.FingerType.TYPE_INDEX].TipPosition);
-                    DebugCube.transform.position = ih.leapHand.Fingers[(int)Finger.FingerType.TYPE_INDEX].TipPosition.ToVector3();
-                }
-            }
+            //Debug.Log("Oculus Remote Button 1");
+            SetPanelDirection();
         }
-        */
+
+        if(Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            //Debug.Log("Oculus Remote Button 2");
+        }
+        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            DistanceFromEye += 0.004f;
+            SetPanelPosition();
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            DistanceFromEye -= 0.004f;
+            SetPanelPosition();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Restart();
+        }
     }
 
+    public void LogTimeAndDistance(float dist)
+    {
+        EndTime = ElapsedTime;
+        TimeList.Add(EndTime - StartTime);
+        DistanceList.Add(dist);
+        ActivateRandomTarget();
+        if(TimeList.Count >= NumberOfTargets || DistanceList.Count >= NumberOfTargets)
+        {
+            string outputFile = string.Format("{0:yyyy-MM-dd_HH-mm-ss}",DateTime.Now);
+            outputFile = outputFile + ".txt";
+            StreamWriter swr = File.CreateText(outputFile);
+            for(int i = 0; i < NumberOfTargets; ++i)
+            {
+                swr.WriteLine ("{0}.  Time : {1} ,  Accuracy : {2}", (i+1), TimeList[i], DistanceList[i]);
+                //Debug.Log((i+1) + ". " + " Time : " + TimeList[i] + " , Accuracy : " + DistanceList[i]);
+            }
+            swr.Close();
+        }
+    }
     private void OnDrawGizmos()
     {
         //Gizmos.DrawRay(CenterEyeAnchor.transform.position, CenterEyeAnchor.transform.forward * 5000.0f);
